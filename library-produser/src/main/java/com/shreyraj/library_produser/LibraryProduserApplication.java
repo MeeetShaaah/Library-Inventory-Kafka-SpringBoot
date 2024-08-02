5da84cdc9d292265a7f11eab973ce1bdddd33801
MeeetShaaah <meetshah7199@gmail.com>
2024-08-01 22:48:25 -0400

    Start with kafka - Springboot library inventory Project.

---
+++ library-produser/src/main/java/com/shreyraj/library_produser/LibraryProduserApplication.java
@@ -0,0 +1,13 @@
+package com.shreyraj.library_produser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProduserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProduserApplication.class, args);
+	}
+
+}
---
+++ library-produser/src/test/java/com/shreyraj/library_produser/LibraryProduserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyraj.library_produser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProduserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
