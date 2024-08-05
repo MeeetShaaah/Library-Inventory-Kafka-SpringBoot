3792885ca3318ef3cd8c3406e3d93acdbc642d1f
MeeetShaaah <meetshah7199@gmail.com>
2024-08-04 22:48:46 -0400

    Start Working with Library - Consumer.

---
+++ library-consumer/src/main/java/Learn/kafka/springboot/LibraryConsumerApplication.java
@@ -0,0 +1,13 @@
+package Learn.kafka.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryConsumerApplication.class, args);
+	}
+
+}
